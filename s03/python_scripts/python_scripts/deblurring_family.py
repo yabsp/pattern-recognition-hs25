0fa2a4a8d27bd56e5f41240cf322ef8828d9a5b1
Yanick Spichty <yanick.spichty@gmail.com>
2025-11-20 09:39:19 +0100

    Implement image wise random filtering in training loop

--- s03/python_scripts/python_scripts/deblurring_family.py
+++ s03/python_scripts/python_scripts/deblurring_family.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 
 import importlib
 import helper
@@ -15,7 +15,7 @@ import torch.optim as optim
 from helper import data_generator, plot_image, add_noise, CNN_simple, CNN_medium, SNR, convolution_fft_torch, Gaussian_blur
 
 use_cuda=torch.cuda.is_available()
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu") 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 #device = "cpu"
 
 print("Device:",device)
@@ -31,16 +31,29 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=1e-3)
 
 for epoch in range(num_epochs):
-    # running_loss = 0.0
+    running_loss = 0.0
+    print(f'epoch: {epoch}')
     sigma_blur = np.random.uniform(0.5, 2.5)
-    kernel = Gaussian_blur(sigma_blur, image_size)
-    kernel_torch = torch.from_numpy(kernel).float().to(device)
+    # kernel = Gaussian_blur(sigma_blur, image_size)
+    # kernel_torch = torch.from_numpy(kernel).float().to(device)
 
     for imgs, _ in trainloader:
         imgs = imgs.to(device)
 
-        conv_imgs = convolution_fft_torch(imgs, kernel_torch)
+        # (2)
+        # conv_imgs = convolution_fft_torch(imgs, kernel_torch)
+        # noisy_imgs = torch.clamp(add_noise(conv_imgs, sigma_noise), 0.0, 1.0)
+
+        # (1)
+        conv_imgs = torch.zeros_like(imgs)
+        for i in range(imgs.shape[0]):
+            sigma_blur = np.random.uniform(0.5, 2.5)
+            kernel = Gaussian_blur(sigma_blur, image_size)
+            kernel_torch = torch.from_numpy(kernel).float().to(device)
+            conv_imgs[i:i + 1] = convolution_fft_torch(imgs[i:i + 1], kernel_torch)
         noisy_imgs = torch.clamp(add_noise(conv_imgs, sigma_noise), 0.0, 1.0)
+
+        outputs = net(noisy_imgs)
 
         optimizer.zero_grad()
         outputs = net(noisy_imgs)
@@ -52,10 +65,10 @@ for epoch in range(num_epochs):
 
 model_scripted = torch.jit.script(net.cpu())
 model_scripted.save("deblurring_family.pt")
+# (1): 230s runtime, ~18.7 - 5.7 SNR
+# (2): 60s runtime, same SNR
+# (3): 788s runtime
 
-
-model_scripted = torch.jit.script(net.cpu())
-model_scripted.save("deblurring_family.pt")
 
 if __name__ == '__main__':
     batch_size = 16
@@ -70,51 +83,51 @@ if __name__ == '__main__':
     image_size = images.shape[2]
 
     # Display one blur
-    kernel = Gaussian_blur(sigma_blur_max*0.5+sigma_blur_min*0.5,image_size=image_size)
-    kernel_torch = torch.from_numpy(kernel).to(device).reshape(1,1,image_size,image_size).type(torch.float32)
+    kernel = Gaussian_blur(sigma_blur_max * 0.5 + sigma_blur_min * 0.5, image_size=image_size)
+    kernel_torch = torch.from_numpy(kernel).to(device).reshape(1, 1, image_size, image_size).type(torch.float32)
     plt.imshow(kernel)
 
     # Display one blured image
-    im_blur_torch = add_noise(convolution_fft_torch(images.to(device),kernel_torch),sigma_noise)
+    im_blur_torch = add_noise(convolution_fft_torch(images.to(device), kernel_torch), sigma_noise)
 
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 6))
-    ax[0].imshow(images[0,0].detach().cpu().numpy())
+    ax[0].imshow(images[0, 0].detach().cpu().numpy())
     ax[0].set_title("Clean")
-    ax[1].imshow(im_blur_torch[0,0].detach().cpu())
+    ax[1].imshow(im_blur_torch[0, 0].detach().cpu())
     ax[1].set_title("Blury")
 
-
-    # Load the neural network    
-    net = torch.jit.load("deblurring_family.pt",map_location=device)
-    print('---> Number of trainable parameters of supercnn: {}'.format(sum(p.numel() for p in net.parameters() if p.requires_grad)))
+    # Load the neural network
+    net = torch.jit.load("deblurring_family.pt", map_location=device)
+    print('---> Number of trainable parameters of supercnn: {}'.format(
+        sum(p.numel() for p in net.parameters() if p.requires_grad)))
 
     Ntest = len(testloader.dataset)
-    image_test_denoised = np.zeros((Ntest,image_size,image_size))
-    image_test_noisy = np.zeros((Ntest,image_size,image_size))
-    image_test_clean = np.zeros((Ntest,image_size,image_size))
+    image_test_denoised = np.zeros((Ntest, image_size, image_size))
+    image_test_noisy = np.zeros((Ntest, image_size, image_size))
+    image_test_clean = np.zeros((Ntest, image_size, image_size))
     SNR_est = np.zeros(Ntest)
     SNR_noise = np.zeros(Ntest)
     cpt = 0
     for i, data in enumerate(testloader, 0):
         img_clean, _ = data
         img_clean = img_clean.to(device)
-        sigma_blur = np.random.rand()*(sigma_blur_max-sigma_blur_min)+sigma_blur_min
-        kernel = Gaussian_blur(sigma_blur,image_size=image_size)
-        kernel_torch = torch.from_numpy(kernel).to(device).reshape(1,1,image_size,image_size).type(torch.float32)
-        img_noisy = add_noise(convolution_fft_torch(img_clean,kernel_torch),sigma_noise)
+        sigma_blur = np.random.rand() * (sigma_blur_max - sigma_blur_min) + sigma_blur_min
+        kernel = Gaussian_blur(sigma_blur, image_size=image_size)
+        kernel_torch = torch.from_numpy(kernel).to(device).reshape(1, 1, image_size, image_size).type(torch.float32)
+        img_noisy = add_noise(convolution_fft_torch(img_clean, kernel_torch), sigma_noise)
         outputs = net(img_noisy)
 
         # save as numpy array
         for k in range(img_clean.shape[0]):
-            image_test_denoised[cpt] = outputs[k].detach().cpu().numpy().reshape(image_size,image_size)
-            image_test_noisy[cpt] = img_noisy[k,0].detach().cpu().numpy()
-            image_test_clean[cpt] = img_clean[k,0].detach().cpu().numpy()
-            SNR_est[cpt] = SNR(img_clean[k:k+1,0].detach().cpu().numpy(),outputs[k:k+1].detach().cpu().numpy())
-            SNR_noise[cpt] = SNR(img_clean[k:k+1,0].detach().cpu().numpy(),img_noisy[k:k+1,0].detach().cpu().numpy())
+            image_test_denoised[cpt] = outputs[k].detach().cpu().numpy().reshape(image_size, image_size)
+            image_test_noisy[cpt] = img_noisy[k, 0].detach().cpu().numpy()
+            image_test_clean[cpt] = img_clean[k, 0].detach().cpu().numpy()
+            SNR_est[cpt] = SNR(img_clean[k:k + 1, 0].detach().cpu().numpy(), outputs[k:k + 1].detach().cpu().numpy())
+            SNR_noise[cpt] = SNR(img_clean[k:k + 1, 0].detach().cpu().numpy(),
+                                 img_noisy[k:k + 1, 0].detach().cpu().numpy())
             cpt += 1
-    print("Average SNR on reconstruction: {} || SNR on noisy images: {}".format(np.mean(SNR_est),np.mean(SNR_noise)))
-        
-    
+    print("Average SNR on reconstruction: {} || SNR on noisy images: {}".format(np.mean(SNR_est), np.mean(SNR_noise)))
+
     index = np.random.randint(Ntest)
     fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(10, 6))
     ax[0].imshow(image_test_clean[index])
